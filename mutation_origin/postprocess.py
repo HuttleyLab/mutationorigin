b58b2e38c7d548c1c622a446ceefb3d933d52472
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2018-11-27 14:25:11 +1100

    MAINT: more consistent usage of single quotes

--- mutation_origin/postprocess.py
+++ mutation_origin/postprocess.py
@@ -6,23 +6,23 @@ from sklearn.metrics import (confusion_matrix,
 from mutation_origin.encoder import (inverse_transform_response,
                                      transform_response)
 
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2014, Gavin Huttley"
-__credits__ = ["Yicheng Zhu", "Cheng Soon Ong", "Gavin Huttley"]
-__license__ = "BSD"
-__version__ = "0.3"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "Development"
+__author__ = 'Gavin Huttley'
+__copyright__ = 'Copyright 2014, Gavin Huttley'
+__credits__ = ['Yicheng Zhu', 'Cheng Soon Ong', 'Gavin Huttley']
+__license__ = 'BSD'
+__version__ = '0.3'
+__maintainer__ = 'Gavin Huttley'
+__email__ = 'Gavin.Huttley@anu.edu.au'
+__status__ = 'Development'
 
 
 def measure_performance(orig, predicted, label_col):
-    """returns dict of classifier performance measures
+    '''returns dict of classifier performance measures
 
     Arguments:
     - orig: DataFrame of original data
     - predicted: DataFrame of predictions
-    - label_col: column containing the labels"""
+    - label_col: column containing the labels'''
     orig = orig[['varid', label_col]]
     orig.set_index('varid', inplace=True)
     predicted.set_index('varid', inplace=True)
@@ -43,14 +43,14 @@ def measure_performance(orig, predicted, label_col):
                                  index=response_labels,
                                  columns=response_labels)
     cf_matrix = cf_matrix.rename_axis('actual / predicted', axis=1)
-    result["confusion_matrix"] = cf_matrix.to_dict(orient='list')
+    result['confusion_matrix'] = cf_matrix.to_dict(orient='list')
     result['balanced_accuracy'] = balanced_accuracy_score(expect, predict)
-    result["auc"] = roc_auc_score(expect, new['scores'])
-    result["mean_precision"] = average_precision_score(expect, new['scores'])
+    result['auc'] = roc_auc_score(expect, new['scores'])
+    result['mean_precision'] = average_precision_score(expect, new['scores'])
 
     precision, recall, thresholds = precision_recall_curve(
         expect, new['scores'])
-    result["prcurve"] = dict(precision=precision.tolist(),
+    result['prcurve'] = dict(precision=precision.tolist(),
                              recall=recall.tolist(),
                              thresholds=thresholds.tolist())
     return result
--- mutation_origin/util.py
+++ mutation_origin/util.py
@@ -11,28 +11,28 @@ from cogent3.util.misc import open_, get_format_suffixes
 from cogent3 import LoadTable
 
 
-__author__ = "Gavin Huttley"
-__copyright__ = "Copyright 2014, Gavin Huttley"
-__credits__ = ["Yicheng Zhu", "Cheng Soon Ong", "Gavin Huttley"]
-__license__ = "BSD"
-__version__ = "0.3"
-__maintainer__ = "Gavin Huttley"
-__email__ = "Gavin.Huttley@anu.edu.au"
-__status__ = "Development"
+__author__ = 'Gavin Huttley'
+__copyright__ = 'Copyright 2014, Gavin Huttley'
+__credits__ = ['Yicheng Zhu', 'Cheng Soon Ong', 'Gavin Huttley']
+__license__ = 'BSD'
+__version__ = '0.3'
+__maintainer__ = 'Gavin Huttley'
+__email__ = 'Gavin.Huttley@anu.edu.au'
+__status__ = 'Development'
 
 
-FILENAME_PATTERNS = {"sample_data": dict(train="train-*.tsv.gz",
-                                         test="test-*.tsv.gz"),
-                     "train": "*-classifier-*.pkl*",
-                     "predict": "*-predicted-*.json.gz",
-                     "performance": "*-performance.json.gz"}
+FILENAME_PATTERNS = {'sample_data': dict(train='train-*.tsv.gz',
+                                         test='test-*.tsv.gz'),
+                     'train': '*-classifier-*.pkl*',
+                     'predict': '*-predicted-*.json.gz',
+                     'performance': '*-performance.json.gz'}
 MUTATION_DIRECTIONS = ('AtoC', 'AtoG', 'AtoT', 'CtoA', 'CtoG', 'CtoT',
                        'GtoA', 'GtoC', 'GtoT', 'TtoA', 'TtoC', 'TtoG')
-BASES = tuple("ACGT")
+BASES = tuple('ACGT')
 
 
 def exec_command(cmnd, stdout=PIPE, stderr=PIPE):
-    """executes shell command and returns stdout if completes exit code 0
+    '''executes shell command and returns stdout if completes exit code 0
 
     Parameters
     ----------
@@ -41,12 +41,12 @@ def exec_command(cmnd, stdout=PIPE, stderr=PIPE):
       shell command to be executed
     stdout, stderr : streams
       Default value (PIPE) intercepts process output, setting to None
-      blocks this."""
+      blocks this.'''
     proc = Popen(cmnd, shell=True, stdout=stdout, stderr=stderr)
     out, err = proc.communicate()
     if proc.returncode != 0:
         msg = err
-        sys.stderr.writelines("FAILED: %s\n%s" % (cmnd, msg))
+        sys.stderr.writelines('FAILED: %s\n%s' % (cmnd, msg))
         sys.exit(proc.returncode)
     if out is not None:
         r = out.decode('utf8')
@@ -56,7 +56,7 @@ def exec_command(cmnd, stdout=PIPE, stderr=PIPE):
 
 
 def get_enu_germline_sizes(total, enu_ratio):
-    """returns the enu and germline sample sizes to satisfy enu_ratio"""
+    '''returns the enu and germline sample sizes to satisfy enu_ratio'''
     unit = total / (enu_ratio + 1)
     enu = int(unit * enu_ratio)
     germline = total - enu
@@ -69,55 +69,55 @@ def valid_response_values(data):
 
 
 def dump_json(path, data):
-    """dumps data in json format"""
-    with open_(path, mode="wt") as outfile:
+    '''dumps data in json format'''
+    with open_(path, mode='wt') as outfile:
         json.dump(data, outfile)
 
 
 def load_json(path):
-    """loads raw data object from json file"""
+    '''loads raw data object from json file'''
     with open_(path) as infile:
         data = json.load(infile)
     return data
 
 
 def load_predictions(path):
-    """returns dataframe, params from  json format prediction data"""
+    '''returns dataframe, params from  json format prediction data'''
     data = load_json(path)
 
-    params = data["feature_params"]
-    df = pandas.DataFrame(data["predictions"])
-    cpath = data.get("classifier_path", None)
-    label = data.get("classifier_label", None)
+    params = data['feature_params']
+    df = pandas.DataFrame(data['predictions'])
+    cpath = data.get('classifier_path', None)
+    label = data.get('classifier_label', None)
     return df, params, cpath, label
 
 
 def load_classifier(path):
-    """returns dict of pickled classifier and features info"""
+    '''returns dict of pickled classifier and features info'''
     with open(path, 'rb') as clf:
         classifier = pickle.load(clf)
     try:
-        feature_params = classifier["feature_params"]
+        feature_params = classifier['feature_params']
         scaler = classifier.get('scaler', None)
-        classifier = classifier["classifier"]
+        classifier = classifier['classifier']
     except KeyError:
-        raise ValueError("pickle formatted file does not "
-                         "contain classifier")
+        raise ValueError('pickle formatted file does not '
+                         'contain classifier')
     return classifier, feature_params, scaler
 
 
 def get_basename(path):
-    """returns a file basename without the suffixes"""
+    '''returns a file basename without the suffixes'''
     bn = os.path.basename(path)
     suffix, cmp_suffix = get_format_suffixes(bn)
-    rindex = bn.rfind(f".{suffix}")
+    rindex = bn.rfind(f'.{suffix}')
     return bn[:rindex]
 
 
 def get_classifier_label(classifier):
-    """returns string label of classifier"""
+    '''returns string label of classifier'''
     name = classifier.__class__.__name__.lower()
-    if "logistic" in name:
+    if 'logistic' in name:
         label = 'lr'
     elif 'nb' in name:
         label = 'nb'
@@ -126,15 +126,15 @@ def get_classifier_label(classifier):
     elif name.startswith('xgb'):
         label = 'xgb'
     else:
-        raise ValueError(f"Unknown classifier type {name}")
+        raise ValueError(f'Unknown classifier type {name}')
     return label
 
 
 def dirname_from_features(features):
-    """generates directory names from a feature set"""
-    dirname = f"f{features['flank_size']}"
+    '''generates directory names from a feature set'''
+    dirname = f'f{features["flank_size"]}'
     if features.get('feature_dim'):
-        dirname += f"d{features['feature_dim']}"
+        dirname += f'd{features["feature_dim"]}'
     if features.get('proximal'):
         dirname += 'p'
     if features.get('usegc'):
@@ -144,7 +144,7 @@ def dirname_from_features(features):
 
 def flank_dim_combinations(max_flank=4, start_flank=0, flank_sizes=None,
                            get_dims=None):
-    """returns flank_size/dim combinations"""
+    '''returns flank_size/dim combinations'''
     combinations = []
     flank_sizes = flank_sizes or range(start_flank, max_flank)
     for fz in flank_sizes:
@@ -163,11 +163,11 @@ def flank_dim_combinations(max_flank=4, start_flank=0, flank_sizes=None,
     return combinations
 
 
-_size = re.compile(r"(?<=/)\d+(?=k/)")
+_size = re.compile(r'(?<=/)\d+(?=k/)')
 
 
 def sample_size_from_path(path):
-    """returns component of path ijndicating sample size"""
+    '''returns component of path ijndicating sample size'''
     try:
         size = int(_size.findall(path)[0]) * 1000
     except IndexError:
@@ -175,17 +175,17 @@ def sample_size_from_path(path):
     return size
 
 
-_rep = re.compile(r"(?<=-)\d+(?=[-.])")
+_rep = re.compile(r'(?<=-)\d+(?=[-.])')
 
 
 def data_rep_from_path(src, path):
-    """returns component of path indicating sample size"""
+    '''returns component of path indicating sample size'''
     basename = os.path.basename(path)
     rep = _rep.findall(basename)[0]
     return rep
 
 
-_feats = re.compile(r"f\d+[d\d]*p*[GC]*")
+_feats = re.compile(r'f\d+[d\d]*p*[GC]*')
 
 
 def feature_set_from_path(path):
@@ -194,28 +194,28 @@ def feature_set_from_path(path):
 
 
 def model_name_from_features(flank, dim, usegc, proximal):
-    """returns the model name from a feature set"""
+    '''returns the model name from a feature set'''
     if flank == 0:
-        model = ["M"]
+        model = ['M']
     elif flank and dim == 1:
-        model = ["M", "I"]
+        model = ['M', 'I']
     elif flank and dim == 2 * flank:
         assert not proximal
-        model = ["FS"]
+        model = ['FS']
     elif dim > 1:
-        prox = "p" if proximal else ""
-        model = ["M", "I"] + [f"{i}D{prox}" for i in range(2, dim + 1)]
+        prox = 'p' if proximal else ''
+        model = ['M', 'I'] + [f'{i}D{prox}' for i in range(2, dim + 1)]
     else:
-        raise ValueError("Unexpected model", flank, flank)
+        raise ValueError('Unexpected model', flank, flank)
     if usegc:
-        model.append("GC")
+        model.append('GC')
 
-    name = "+".join(model)
+    name = '+'.join(model)
     return name
 
 
 def summary_stat_table(table, factors):
-    """returns summary statistics for classifier, feature set combination"""
+    '''returns summary statistics for classifier, feature set combination'''
     fscore_cols = [c for c in table.header if c.startswith('fscore')]
     distinct = table.distinct_values(factors)
     rows = []
@@ -234,19 +234,19 @@ def summary_stat_table(table, factors):
             row.append(data.std(ddof=1))
         rows.append(row)
 
-    header = list(factors) + ["mean_auc", "std_auc", "mean_ap", "std_ap",
+    header = list(factors) + ['mean_auc', 'std_auc', 'mean_ap', 'std_ap',
                               'mean_balanced_accuracy',
                               'std_balanced_accuracy']
     for col in fscore_cols:
         header.extend([f'mean_{col}', f'std_{col}'])
 
     table = LoadTable(header=header, rows=rows)
-    table = table.sorted(reverse="mean_auc")
+    table = table.sorted(reverse='mean_auc')
     return table
 
 
 def iter_indices(total, block_size):
-    """yields a block_size numpy array of indices up to total"""
+    '''yields a block_size numpy array of indices up to total'''
     for start in range(0, total, block_size):
         end = min(total, start + block_size)
         indices = range(start, end)
@@ -254,11 +254,10 @@ def iter_indices(total, block_size):
 
 
 def skip_path(exclude_paths, path):
-    """returns True if path has a match to exclude paths"""
+    '''returns True if path has a match to exclude paths'''
     result = False
     for ex in exclude_paths:
         if ex in path:
             result = True
             break
     return result
-
