a71a21386e4b906a161a5f953f06d88ebac45741
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2018-11-27 14:13:56 +1100

    MAINT: now explicitly specify solver in logistic regression and increase maxiter
    Issue due to changes in scikit-learn 0.2

--- mutation_origin/classify.py
+++ mutation_origin/classify.py
@@ -27,7 +27,8 @@ def logistic_regression(feat, resp, seed, c_values, penalty_options, n_jobs):
     """fits a logistic regression classifier using a grid search with CV"""
     param_grid = {'C': c_values, 'penalty': penalty_options}
     shuffle_split = ShuffleSplit(n_splits=1, test_size=0.5, random_state=seed)
-    log_reg = LogisticRegression(class_weight='balanced')
+    log_reg = LogisticRegression(class_weight='balanced', solver='liblinear',
+                                 max_iter=1000)
     classifier = GridSearchCV(estimator=log_reg, param_grid=param_grid,
                               scoring='roc_auc',
                               cv=shuffle_split,
