1ed06d68433bc772a9faf4c855fcf6e86808dddc
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-02-28 08:23:06 +1100

    BUG: fixed typo in specifying auc scoring classifier
    Only effects changes after recent introduction of a choice in scoring metric

--- mutation_origin/opt.py
+++ mutation_origin/opt.py
@@ -157,7 +157,7 @@ _excludes = click.option("-ex", "--exclude_paths",
                          type=click.Path(),
                          help="Paths to exclude, comma separated")
 _score = click.option('-sc', '--scoring',
-                         type=click.Choice(['au_roc', 'balanced_accuracy',
-                                            'f1']),
-                         default='au_roc',
-                         help="Scoring for classifier performance")
+                      type=click.Choice(['roc_auc', 'balanced_accuracy',
+                                         'f1']),
+                      default='roc_auc',
+                      help="Scoring for classifier performance")
