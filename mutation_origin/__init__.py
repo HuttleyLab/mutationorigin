19815f49fe52b27730e0d17e12c8e398b02d559d
Gavin Huttley <Gavin.Huttley@anu.edu.au>
2019-09-25 15:13:26 +1000

    ENH: added module __init__

---
+++ mutation_origin/__init__.py
@@ -0,0 +1,15 @@
+from warnings import filterwarnings
+
+filterwarnings("ignore", "Data with input dtype int8 was converted")
+filterwarnings("ignore", "Precision and F-score")
+filterwarnings("ignore", "Trying to unpickle estimator")
+
+
+__author__ = "Gavin Huttley"
+__copyright__ = "Copyright 2014, Gavin Huttley"
+__credits__ = ["Yicheng Zhu", "Cheng Soon Ong", "Gavin Huttley"]
+__license__ = "BSD"
+__version__ = "0.3"
+__maintainer__ = "Gavin Huttley"
+__email__ = "Gavin.Huttley@anu.edu.au"
+__status__ = "Development"
